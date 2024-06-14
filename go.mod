69d213b0287d6dd3747b47ec4e89885d57d3c295
dongwlin <linsann@163.com>
2024-06-14 12:52:27 +0800

    feat: init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dongwlin/maa-framework-go-test
+
+go 1.22
+
+require github.com/MaaXYZ/maa-framework-go v0.2.1 // indirect
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MaaXYZ/maa-framework-go"
+	"path/filepath"
+)
+
+func main() {
+	maa.SetLogDir("debug")
+	maa.SetSaveDraw(true)
+	maa.SetStdoutLevel(maa.LoggingLevelInfo)
+
+	testingDataSetDir := "TestingDataSet"
+	testingPath := filepath.Join(testingDataSetDir, "PipelineSmoking", "MaaRecording.txt")
+	resultPath := filepath.Join(testingDataSetDir, "debug")
+
+	ctrl := maa.NewDbgController(testingPath, resultPath, maa.DbgControllerTypeReplayRecording, "{}", nil)
+	defer ctrl.Destroy()
+	ctrlId := ctrl.PostConnect()
+
+	res := maa.NewResource(nil)
+	defer res.Destroy()
+	resDir := filepath.Join(testingDataSetDir, "PipelineSmoking", "resource")
+	resId := res.PostPath(resDir)
+
+	ctrl.Wait(ctrlId)
+	res.Wait(resId)
+
+	inst := maa.New(nil)
+	defer inst.Destroy()
+	inst.BindResource(res)
+	inst.BindController(ctrl)
+
+	if !inst.Inited() {
+		fmt.Println("failure")
+		return
+	}
+
+	taskId := inst.PostTask("Wilderness", "{}")
+	stats := inst.WaitTask(taskId)
+	if stats == maa.Success {
+		fmt.Println("success")
+	} else {
+		fmt.Println("failure")
+	}
+}
